d133f865dff70d837038bcdf87f0c4bfcf084947
FeraVerto <themightymasha@gmail.com>
2023-06-26 18:54:21 +0400

    added sidebar for tags

--- src/Pages/Tags/Tags.tsx
+++ src/Pages/Tags/Tags.tsx
@@ -1,19 +1,31 @@
+//libraries
+import { ReactElement, useState } from 'react';
 //style
 import stl from './Tags.module.css';
 //временно
 import { tags } from '../../temp';
-import { ReactElement } from 'react';
 
 export const Tags = (): ReactElement => {
+  const [isOpen, setOpen] = useState(false);
+  const tagsItems = tags.map((m) => (
+    <li className={stl.tags_item} key={m.id}>
+      m.name
+    </li>
+  ));
+
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  };
+
   return (
-    <div className={`${stl.tags} ${stl.fixed_block}`}>
-      <ul className={stl.tags_list}>
-        {tags.map((m) => (
-          <li className={stl.tags_item} key={m.id}>
-            m.name
-          </li>
-        ))}
-      </ul>
+    <div className={`${stl.sidebar_tags} ${isOpen ? `${stl.open}` : ''}`}>
+      <ul className={stl.sidebar_tags_item}>{tagsItems}</ul>
+      <div
+        className={`${stl.toggle_button} ${isOpen ? `${stl.open}` : ''}`}
+        onClick={toggleMenu}
+      >
+        <span>{isOpen ? 'Закрыть тэги' : 'Открыть тэги'}</span>
+      </div>
     </div>
   );
 };
